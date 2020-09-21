3653133791db7b5af8c19b52537081b7938abca3
Hashy-0x43 <Daemon84@protonmail.ch>
2020-09-21 00:12:18 -0400

    Update pin_check.c

--- pin_check.c
+++ pin_check.c
@@ -3,7 +3,7 @@
 int check();
 
 int check(int input) {
-          int pin = 27960;
+          int pin = 27960 / 15;
           
           if(input != pin) {
                     printf("Wrong\n");
@@ -18,7 +18,7 @@ int main(int argc,char **argv[1]) {
           int x = argv[1];
           
           if(argc != 2) {
-                    printf("pin_check <ping>\n");
+                    printf("pin_check <pin>\n");
           }
           
           if(argc == 2) {
